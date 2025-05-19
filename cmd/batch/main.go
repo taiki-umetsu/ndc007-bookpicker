ad4d88678e2e5c135e08415ea0019085179ee5fe
taiki-umetsu <taiki.umetsu.st@gmail.com>
2025-05-19 13:35:28 +0900

    measure batch processing time

--- cmd/batch/main.go
+++ cmd/batch/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/taiki-umetsu/ndc007-bookpicker/internal/cinii"
 	"github.com/taiki-umetsu/ndc007-bookpicker/internal/database"
@@ -16,6 +17,8 @@ import (
 )
 
 func main() {
+	startTime := time.Now()
+
 	if os.Getenv("GO_ENV") != "production" {
 		if err := godotenv.Load(".env.development"); err != nil {
 			log.Println("Warning: .env.development ファイルの読み込みに失敗しました:", err)
@@ -52,18 +55,19 @@ func main() {
 
 	const (
 		yearFrom = 2020
-		count    = 10
+		//count    = 10
+		count = 1
 	)
 
 	// 参考: https://www.ndl.go.jp/jp/data/NDC10code201708.pdf
 	ndcList := []string{
-		"007",     // 情報学．情報科学
-		"007.3*",  // 情報と社会：情報政策，情報倫理
-		"007.6",   // データ処理．情報処理
-		"007.609", // データ管理：データセキュリティ，データマイニング
-		"007.61",  // システム分析．システム設計．システム開発
-		"007.63",  // コンピュータシステム．ソフトウェア．ミドルウェア．アプリケーション
-		"007.64",  // コンピュータプログラミング
+		"007",    // 情報学．情報科学
+		"007.3*", // 情報と社会：情報政策，情報倫理
+		// "007.6",   // データ処理．情報処理
+		// "007.609", // データ管理：データセキュリティ，データマイニング
+		// "007.61",  // システム分析．システム設計．システム開発
+		// "007.63",  // コンピュータシステム．ソフトウェア．ミドルウェア．アプリケーション
+		// "007.64",  // コンピュータプログラミング
 	}
 
 	var isbns []string
@@ -121,4 +125,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("❌ 書き込み失敗: %v", err)
 	}
+
+	elapsedTime := time.Since(startTime)
+	log.Printf("[complete] バッチ処理時間: %s", elapsedTime)
 }
